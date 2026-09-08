4827c3e0cee82a57c92e83d9fbd64177d4a71d7a
Devandroid-bit <180217512+Devandroid-bit@users.noreply.github.com>
2026-09-08 02:32:47 +0100

    Delete unica/patches/saiv directory

--- unica/patches/saiv/customize.sh
+++
@@ -1,8 +0,0 @@
-SOURCE_FIRMWARE_PATH="$(cut -d "/" -f 1 -s <<< "$SOURCE_FIRMWARE")_$(cut -d "/" -f 2 -s <<< "$SOURCE_FIRMWARE")"
-TARGET_FIRMWARE_PATH="$(cut -d "/" -f 1 -s <<< "$TARGET_FIRMWARE")_$(cut -d "/" -f 2 -s <<< "$TARGET_FIRMWARE")"
-
-# SEC_PRODUCT_FEATURE_GALLERY_CONFIG_PET_CLUSTER_VERSION
-LOG_STEP_IN "- Adding SAIV pet clustering databases"
-ADD_TO_WORK_DIR "$SOURCE_FIRMWARE_PATH" "vendor" "saiv/image_understanding/db/pet_detector" 0 2000 755 "u:object_r:vendor_snap_file:s0"
-ADD_TO_WORK_DIR "$SOURCE_FIRMWARE_PATH" "vendor" "saiv/image_understanding/db/pet_mypetsearch" 0 2000 755 "u:object_r:vendor_snap_file:s0"
-LOG_STEP_OUT
