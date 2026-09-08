fb58c550211efff993eb3fed122f43b7592288b2
Devandroid-bit <180217512+Devandroid-bit@users.noreply.github.com>
2026-09-08 22:09:29 +0100

    unica: patches: stock_blobs: add a new set of patches
    Author and credits: @ArtisanROM.

--- unica/patches/stock_blobs/customize.sh
+++ unica/patches/stock_blobs/customize.sh
@@ -1,3 +1,25 @@
+MATCH_TARGET_FEATURES()
+{
+    local SOURCE_FEATURES
+    local TARGET_FEATURES
+
+    SOURCE_FEATURES="$(find "$WORK_DIR/system/system/etc/permissions" -name "com.sec.feature*" -printf "%f\n")"
+    SOURCE_FEATURES="$(sort <<< "$SOURCE_FEATURES")"
+    TARGET_FEATURES="$(find "$FW_DIR/$TARGET_FIRMWARE_PATH/system/system/etc/permissions" -name "com.sec.feature*" -printf "%f\n")"
+    TARGET_FEATURES="$(sort <<< "$TARGET_FEATURES")"
+
+    for f in $SOURCE_FEATURES; do
+        if ! grep -q "$f" <<< "$TARGET_FEATURES"; then
+            DELETE_FROM_WORK_DIR "system" "system/etc/permissions/$f"
+        fi
+    done
+    for f in $TARGET_FEATURES; do
+        if ! grep -q "$f" <<< "$SOURCE_FEATURES"; then
+            ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/permissions/$f" 0 0 644 "u:object_r:system_file:s0"
+        fi
+    done
+}
+
 SOURCE_FIRMWARE_PATH="$FW_DIR/$(echo -n "$SOURCE_FIRMWARE" | sed 's./._.g' | rev | cut -d "_" -f2- | rev)"
 TARGET_FIRMWARE_PATH="$FW_DIR/$(echo -n "$TARGET_FIRMWARE" | sed 's./._.g' | rev | cut -d "_" -f2- | rev)"
 
@@ -18,6 +40,32 @@ ADD_TO_WORK_DIR "$SOURCE_FIRMWARE_PATH" "system" "system/cameradata/preloadfilte
 DELETE_FROM_WORK_DIR "system" "system/cameradata/myfilter"
 ADD_TO_WORK_DIR "$SOURCE_FIRMWARE_PATH" "system" "system/cameradata/myfilter" 0 0 755 "u:object_r:system_file:s0"
 LOG_STEP_OUT
+
+# TODO add APE/DSD extractor libs if required
+if [ -f "$WORK_DIR/system/system/lib64/extractors/libsapeextractor.so" ] && \
+        [ ! "$(GET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_MMFW_SUPPORT_APE_FORMAT")" ]; then
+    DELETE_FROM_WORK_DIR "system" "system/lib64/extractors/libsapeextractor.so"
+fi
+if [ -f "$WORK_DIR/system/system/lib64/extractors/libsdffextractor.so" ] && \
+        [ ! "$(GET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_MMFW_SUPPORT_DSD_FORMAT")" ]; then
+    DELETE_FROM_WORK_DIR "system" "system/lib64/extractors/libsdffextractor.so"
+fi
+if [ -f "$WORK_DIR/system/system/lib64/extractors/libsdsfextractor.so" ] && \
+        [ ! "$(GET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_MMFW_SUPPORT_DSD_FORMAT")" ]; then
+    DELETE_FROM_WORK_DIR "system" "system/lib64/extractors/libsdsfextractor.so"
+fi
+
+if [ -f "$TARGET_FIRMWARE_PATH/system/system/priv-app/SohService/SohService.apk" ]; then
+    DECODE_APK "system" "system/priv-app/SohService/SohService.apk"
+
+    LOG "- Adding target BSOH blobs"
+    EVAL "rm -r \"$APKTOOL_DIR/system/priv-app/SohService/SohService.apk/assets\""
+    EVAL "unzip -q \"$TARGET_FIRMWARE_PATH/system/system/priv-app/SohService/SohService.apk\" \"assets/*\" -d \"$APKTOOL_DIR/system/priv-app/SohService/SohService.apk\""
+else
+    if [ -f "$WORK_DIR/system/system/priv-app/SohService/SohService.apk" ]; then
+        DELETE_FROM_WORK_DIR "system" "system/priv-app/SohService"
+    fi
+fi
 
 if [ -f "$TARGET_FIRMWARE_PATH/system/system/usr/share/alsa/alsa.conf" ]; then
     LOG_STEP_IN "- Replacing alsa.conf"
